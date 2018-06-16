fdfb38114d1fd5f539f492bee8410e750ae5af8c
jomartinezmarquez@gmail.com <jomartinezmarquez@gmail.com>
2018-06-16 21:31:08 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,5 +1,5 @@
 # Write your #display_rainbow method here
 def display_rainbow(colors)
   puts "#{colors[0][0].upcase}: #{colors[0]}, #{colors[1][0].upcase}: #{colors[1]},
-  #{colors[2][0].upcase}: #{colors[2]}"
+  #{colors[2][0].upcase}: #{colors[2]}, #{colors[3][0].upcase}: #{colors[3]}"
 end
